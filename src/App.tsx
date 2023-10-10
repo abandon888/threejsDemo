6cd98262ae072d83a07929fe99ba95e18b6ffd15
abandon888 <1876746719@qq.com>
2023-10-10 16:18:05 +0800

    feat: complete first-person perspective

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import * as THREE from 'three'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 //import { World } from './World1/World.js'
 import { Leva } from 'leva'
 import Experience from './world/Experience.js'
@@ -8,14 +8,18 @@ import World1 from './world/World.js'
 import Portfoli from './world/Portfolio/index.js'
 import Physics from './world/physics/index.js'
 import GalleryPage from './world/gallery/index.js'
+import { useEffect, useState } from 'react'
+import { KeyboardControls, useKeyboardControls } from '@react-three/drei'
 
 export default function App() {
-  const created = ({ scene }: any) => {
-    scene.background = new THREE.Color('white')
-  }
+  // const created = ({ scene }: any) => {
+  //   scene.background = new THREE.Color('white')
+  // }
+  //添加第一人称控制器
   return (
     <>
       <Leva collapsed></Leva>
+
       <Canvas
         //dir='2'
         // orthographic
@@ -26,13 +30,11 @@ export default function App() {
         // } }
         shadows
         camera={{
-          position: [3, 2, 6],
           //  zoom: 50,
           fov: 45,
           near: 0.1,
           far: 200,
-        }}
-        onCreated={created}>
+        }}>
         {/* <Experience /> */}
         {/* <World1 /> */}
         {/* <ModelExp /> */}
--- src/world/gallery/index.tsx
+++ src/world/gallery/index.tsx
@@ -1,16 +1,101 @@
-import { OrbitControls, useGLTF } from '@react-three/drei'
+import {
+  KeyboardControls,
+  OrbitControls,
+  PointerLockControls,
+  useGLTF,
+  useKeyboardControls,
+} from '@react-three/drei'
+import { useThree, useFrame, Canvas } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
+import { useState, useEffect } from 'react'
+import { extend } from '@react-three/fiber'
+import { Debug, Physics, RigidBody } from '@react-three/rapier'
+import { Player } from './player'
 
 export default function GalleryPage() {
   const gallery = useGLTF('./gallery/scene_collision.gltf')
+  const { camera } = useThree()
+  const [position, setPositon] = useState([0, 0, 0])
+  // const controls = new OrbitControls(camera)
+
+  // //使用useFrame持续更新相机的位置
+  // useFrame((state) => {
+  //   // const position = state.camera.position
+  //   // const quaternion = state.camera.quaternion
+  //   // const euler = state.camera.rotation
+  //   // console.log(position)
+  //   //更新相机的位置
+  //   camera.position.set(position[0], position[1], position[2])
+  //   const controls = new OrbitControls(camera,camera.domElement)
+  // })
+
+  // const changePosition = (e: any) => {
+  //   //根据按下的键盘按键，改变相机的位置,w向前，s向后，a向左，d向右
+  //   switch (e.key) {
+  //     case 'w':
+  //       setPositon([position[0], position[1], position[2] - 1])
+  //       break
+  //     case 's':
+  //       setPositon([position[0], position[1], position[2] + 1])
+  //       break
+  //     case 'a':
+  //       setPositon([position[0] - 1, position[1], position[2]])
+  //       break
+  //     case 'd':
+  //       setPositon([position[0] + 1, position[1], position[2]])
+  //       break
+  //     default:
+  //       break
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener('keydown', changePosition)
+  //   return () => {
+  //     window.removeEventListener('keydown', changePosition)
+  //   }
+  // }, [])
+
+  // useEffect(() => {
+  //   //设置相机的位置
+  //   camera.position.set(position[0], position[1], position[2])
+  // }, [])
+
   return (
     <>
-      <OrbitControls makeDefault />
-      <Perf position="top-left" />
-      <directionalLight castShadow position={[0, 10, 0]} intensity={1} />
-      <ambientLight intensity={0.5} />
-      {/* 引入模型 */}
-      <primitive object={gallery.scene} />;
+      <KeyboardControls
+        map={[
+          { name: 'forward', keys: ['ArrowUp', 'w', 'W'] },
+          { name: 'backward', keys: ['ArrowDown', 's', 'S'] },
+          { name: 'left', keys: ['ArrowLeft', 'a', 'A'] },
+          { name: 'right', keys: ['ArrowRight', 'd', 'D'] },
+          { name: 'jump', keys: ['Space'] },
+        ]}>
+        <Physics>
+          <Debug />
+          {/* <Canvas camera={{ position: [0, 0, 0] }}> */}
+          <Perf position="top-left" />
+          <directionalLight castShadow position={[0, 10, 0]} intensity={1} />
+          <ambientLight intensity={0.5} />
+          {/* 引入模型 */}
+          <Player />
+          <RigidBody type="fixed" colliders="cuboid">
+            <primitive object={gallery.scene} />;{/* </Canvas> */}
+          </RigidBody>
+
+          <RigidBody type="fixed" friction={0}>
+            <mesh
+              receiveShadow
+              position-y={-1.25}
+              rotation-x={-Math.PI * 0.5}
+              scale={10}>
+              <boxGeometry args={[1, 1, 0.05]} />
+              <meshStandardMaterial color="powderblue" />
+            </mesh>
+          </RigidBody>
+        </Physics>
+        <PointerLockControls />
+      </KeyboardControls>
     </>
   )
 }
